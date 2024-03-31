bef5807dc93f0712c0754799651bbf6ac1b48bbd
trakt <trakt@DESKTOP-F1DCND3>
2024-03-31 17:44:20 +0300

    Добавьте файлы проекта.

---
+++ 27.03.2024/27.03.2024.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numbers>
+
+using namespace std;
+
+class cl    //Task2
+{
+public:
+    bool operator()(string& str1, string& str2)
+    {
+        return str1.size() < str2.size();
+    }
+
+};
+
+bool mySort(string& str1, string& str2) //Task1
+{
+    return str1.size() < str2.size();
+}
+
+int main()
+{
+    //Task1
+    vector<string> predicat = { "Hello","How","Areeee" };
+    sort(predicat.begin(), predicat.end(), mySort);
+    for (auto it = predicat.begin(); it != predicat.end(); it++)
+    {
+        cout << *it << endl;
+    }
+    //Task2
+    vector<string> functor = { "Hello", "How", "Areeee"};
+    sort(functor.begin(), functor.end(), cl());
+    for (auto it = functor.begin(); it != functor.end(); it++)
+    {
+        cout << *it << endl;
+    }
+}
